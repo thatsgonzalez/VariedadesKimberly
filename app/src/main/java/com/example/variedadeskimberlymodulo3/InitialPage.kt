65d79d91cc02c127c80587dd3dbbbc95a9cc337c
thatsgonzalez <fernandomelen20@gmail.com>
2023-04-29 00:50:36 -0600

    First Page

--- app/src/main/java/com/example/variedadeskimberlymodulo3/InitialPage.kt
+++ app/src/main/java/com/example/variedadeskimberlymodulo3/InitialPage.kt
@@ -1,11 +1,25 @@
 package com.example.variedadeskimberlymodulo3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class InitialPage : AppCompatActivity() {
+    lateinit var btnLoginCliente: Button
+    lateinit var btnLoginEmpleado: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial_page)
+        btnLoginCliente = findViewById(R.id.btnLoginCliente)
+        btnLoginEmpleado = findViewById(R.id.btnLoginEmpleado)
+
+        btnLoginCliente.setOnClickListener(){
+
+        }
+
+        btnLoginEmpleado.setOnClickListener(){
+
+        }
     }
 }
