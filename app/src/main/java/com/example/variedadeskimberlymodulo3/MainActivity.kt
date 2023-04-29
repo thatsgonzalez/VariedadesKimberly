4d3953f5cf5ae49512509227ec0bff78c2de50c4
thatsgonzalez <fernandomelen20@gmail.com>
2023-04-28 23:34:51 -0600

    fasdf

--- app/src/main/java/com/example/variedadeskimberlymodulo3/MainActivity.kt
+++ app/src/main/java/com/example/variedadeskimberlymodulo3/MainActivity.kt
@@ -11,13 +11,13 @@ import com.example.variedadeskimberlymodulo3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
